8c2ef106b1889ca4e8da2b9fe3142a1b8caafc60
yappy <superunyo@yahoo.co.jp>
2023-01-07 03:51:40 +0900

    Add main binary project

---
+++ src/download.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+#[allow(dead_code)]
+const BULK_TYPE_ORACLE: &str = "oracle_cards";
+#[allow(dead_code)]
+const BULK_TYPE_ARTWORK: &str = "unique_artwork";
+#[allow(dead_code)]
+const BULK_TYPE_DEFAULT: &str = "default_cards";
+#[allow(dead_code)]
+const BULK_TYPE_ALL: &str = "all_cards";
+#[allow(dead_code)]
+const BULK_TYPE_RULINGS: &str = "rulings";
+
+pub fn entry() -> Result<()> {
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod download;
+
+use anyhow::Result;
+use getopts::Options;
+use std::env;
+
+/// Parse args and dispatch.
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let program = &args[0];
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this help");
+    opts.optflag("d", "download", "Download");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(fail) => {
+            eprintln!("{fail}");
+            std::process::exit(1);
+        }
+    };
+
+    let print_help = || {
+        let brief = format!("Usage: {program} [options]");
+        print!("{}", opts.usage(&brief));
+    };
+
+    if matches.opt_present("h") {
+        print_help();
+        std::process::exit(0);
+    }
+
+    if matches.opt_present("d") {
+        download::entry()?;
+    } else {
+        print_help();
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
